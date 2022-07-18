30d83da053d0c88e044f75e477a581e819b5a606
M. Atul Kumar <atul@creativetechnologist.de>
2022-07-18 16:50:25 +0200

    debugging

--- src/features/sc-player/url-params.ts
+++ src/features/sc-player/url-params.ts
@@ -2,9 +2,11 @@ import { AppConfig } from '../../app-types';
 import { parseSearch } from '../../utils/url-utils';
 import { PlayerConfig } from './ScreenCloudPlayerProvider';
 
-
 export function configFromUrlParams(searchParams: URLSearchParams, currentConfig?: PlayerConfig) {
   const params = parseSearch(searchParams);
+
+  if (!params['config']) return currentConfig;
+
   return {
     ...currentConfig,
     ...(params['config'] ? JSON.parse(params['config']) : {}),
