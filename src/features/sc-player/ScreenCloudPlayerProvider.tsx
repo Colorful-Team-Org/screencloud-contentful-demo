03272ddb15b8602cd63219f4c41760c27e03d8cb
M. Atul Kumar <atul@creativetechnologist.de>
2022-07-18 16:55:31 +0200

    cleanup

--- src/features/sc-player/ScreenCloudPlayerProvider.tsx
+++ src/features/sc-player/ScreenCloudPlayerProvider.tsx
@@ -42,16 +42,11 @@ export function ScreenCloudPlayerProvider(props: PropsWithChildren<any>) {
       if (process.env.NODE_ENV === 'development') {
         appConfig = devConfig.config;
       }
-      console.log('appConfig 0', appConfig);
-      appConfig = configFromUrlParams(searchParams, appConfig);
-      console.log('appConfig 1', appConfig);
 
+      appConfig = configFromUrlParams(searchParams, appConfig);
       const sc = await connectScreenCloud<AppConfig>(appConfig ? { config: appConfig } : undefined);
 
-      console.log('appConfig 2', appConfig);
-
       appConfig = sc.getConfig();
-      console.log('appConfig 3', appConfig);
 
       const config: PlayerConfig = {
         ...appConfig,
